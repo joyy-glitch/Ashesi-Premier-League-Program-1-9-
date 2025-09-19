229ac7f472510656c75a0fb989e89fc27fe44ea0
Joy Addy <joyaddy267@gmail.com>
2025-09-19 14:42:07 +0000

    helloo

---
+++ src/FootballPlayer.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FootballPlayer {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        float height;
+        float weight;
+        int jerseyNumber;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter values for player details:");
+
+
+
+           }
+}
